ccdf91166a7b981f131d59f261037d1480a678ce
Omar-Slik <92442894+Omar-Slik@users.noreply.github.com>
2021-11-03 19:46:22 +0100

    new

---
+++ NoteBook/Controllers/NoteBookController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Text;
+
+
+namespace NoteBook.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class NoteBookController : ControllerBase
+    {
+        NoteBook noteBook = new NoteBook();
+        [HttpGet]
+        public string[] GetAllNotes()
+        {
+            return noteBook.GetAllNotes();
+        }
+        [HttpPost]
+        public void TakeNote(string note)
+        {
+            noteBook.TakeNote(note);
+        }
+    }
+}
---
+++ NoteBook/NoteBook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteBook
+{
+    internal class NoteBook
+    {
+        public void TakeNote(string note)
+        {
+
+            if (!File.Exists("notes.txt"))
+            {
+                string[] headerInfo = new string[]
+                {
+                    $"{"Datum",-24}{"Note"}", 
+                    
+                    new string('-', 60)
+                };
+                File.WriteAllLines("notes.txt", headerInfo);
+            }
+            string output = $"{DateTime.Now.ToString(),-24}{note}";
+            File.AppendAllText("notes.txt", output + Environment.NewLine);
+        }
+        public string[] GetAllNotes()
+        {
+            if (!File.Exists("notes.txt"))
+            {
+                string[] headerInfo = new string[]
+                {
+                    $"{"Datum",-24}{"Note"}",
+
+                    new string('-', 60)
+                };
+                File.WriteAllLines("notes.txt", headerInfo);
+            }
+            string[] notes = File.ReadAllLines("notes.txt");
+            return notes[2..];
+        }
+    }
+}
